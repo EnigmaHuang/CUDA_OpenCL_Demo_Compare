680c33bc811b264d27bc1750571e67747460fe30
Enigma Huang <enigmahuang@gmail.com>
2018-01-30 20:00:36 -0500

    Fix indent

--- OpenCL/OpenCL_tools.cpp
+++ OpenCL/OpenCL_tools.cpp
@@ -8,31 +8,31 @@
 // Read kernel file into a string
 int readCLKernelFile(const char *file_name, size_t *file_size, char **file_content) 
 {
-    // Open the file
-    FILE *inf = fopen(file_name, "r");
-    if (inf == NULL) 
+	// Open the file
+	FILE *inf = fopen(file_name, "r");
+	if (inf == NULL) 
 	{
-        printf("-- Error opening file %s\n", file_name);
+		printf("-- Error opening file %s\n", file_name);
 		file_content = NULL;
 		file_size = 0;
-        return 1;
-    }
+		return 1;
+	}
 	
-    // Get its size
-    fseek(inf, 0, SEEK_END);
-    long size = ftell(inf);
+	// Get its size
+	fseek(inf, 0, SEEK_END);
+	long size = ftell(inf);
 	*file_size = (size_t)(size + 1);
-    rewind(inf);
+	rewind(inf);
 
-    // Read the kernel code as a string
-    char *_content = (char*) malloc((size + 1) * sizeof(char));
-    size_t read_source_size = fread(_content, 1, size * sizeof(char), inf);
+	// Read the kernel code as a string
+	char *_content = (char*) malloc((size + 1) * sizeof(char));
+	size_t read_source_size = fread(_content, 1, size * sizeof(char), inf);
 	assert(*file_size == read_source_size + 1);
-    _content[size] = '\0';
+	_content[size] = '\0';
 	*file_content = _content;
 	
-    fclose(inf);
-    return 0;
+	fclose(inf);
+	return 0;
 }
 
 
@@ -51,7 +51,7 @@ int getCLPlatform(cl_platform_id *platform, int platform_id)
 	if (numPlatforms > 0)
 	{
 		cl_platform_id *platforms = (cl_platform_id *) malloc(numPlatforms * sizeof(cl_platform_id));
-		status    = clGetPlatformIDs(numPlatforms, platforms, NULL);
+		status	= clGetPlatformIDs(numPlatforms, platforms, NULL);
 		if (platform_id < numPlatforms)
 		{
 			*platform = platforms[platform_id];
@@ -70,9 +70,9 @@ int getCLPlatform(cl_platform_id *platform, int platform_id)
 // Query the platform and choose all GPU devices
 int getCLGPUDevicesID(cl_platform_id *platform, cl_device_id **device)
 {
-	cl_uint      numDevices = 0;
+	cl_uint	  numDevices = 0;
 	cl_device_id *_device   = NULL;
-	cl_int       status     = clGetDeviceIDs(*platform, CL_DEVICE_TYPE_GPU, 0, NULL, &numDevices);
+	cl_int	   status	 = clGetDeviceIDs(*platform, CL_DEVICE_TYPE_GPU, 0, NULL, &numDevices);
 	if (numDevices > 0) 
 	{
 		_device = (cl_device_id*) malloc(numDevices * sizeof(cl_device_id));
